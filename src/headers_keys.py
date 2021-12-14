e4f8e7e5e2d85df43aba819ab8698f4723ed2d3f
marcos-vas <marcosvaas@gmail.com>
2021-12-14 09:28:58 -0300

    feat: levantamento das colunas

---
+++ src/headers_keys.py
@@ -0,0 +1,44 @@
+CONTRACHEQUE = "contracheque"
+CONTRACHEQUE_2019 = "contracheque_2019"
+INDENIZACOES = "indenizacoes"
+
+HEADERS = {
+    CONTRACHEQUE: {
+        "Remuneração do Cargo Efetivo": 4,
+        "Outras Verbas Remuneratórias Legais/Judiciais": 5,
+        "Função de Confiança ou Cargo em Comissão": 6,
+        "13o. Salário": 7,
+        "Adicional de Férias": 8,
+        "Abono de Permanência": 9,
+        "Outras Remunerações Temporárias": 10,
+        "Verbas Indenizatórias": 11,
+        "Contribuição Previdenciária": 13,
+        "Imposto de Renda": 14,
+        "Retenção do Teto": 15,
+    },
+
+    CONTRACHEQUE_2019: {
+        "Remuneração do Cargo Efetivo": 4,
+        "Outras Verbas Remuneratórias Legais/Judiciais": 5,
+        "Função de Confiança ou Cargo em Comissão": 6,
+        "13o. Salário": 7,
+        "Adicional de Férias": 8,
+        "Abono de Permanência": 9,
+        "Outras Remunerações Temporárias": 10,
+        "Contribuição Previdenciária": 12,
+        "Imposto de Renda": 13,
+        "Retenção do Teto": 14,
+    },
+
+    INDENIZACOES: {
+        "Auxílio alimentação": 4,
+        "Auxílio saúde": 5,
+        "Auxílio Pré Escolar": 6,
+        "Auxílio Cursos": 7,
+        "Auxílio Moradia": 8,
+        "Adicional Noturno": 9,
+        "Serviço Extraor.": 10,
+        "Substituição de Função": 11,
+        "Cumulações": 12,
+    },
+}
